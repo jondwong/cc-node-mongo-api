1965abf2526093864e35b2f85e1f7164ea379859
Jon Wong <jwong112157@gmail.com>
2020-04-24 10:14:41 -0400

    bugfix: corrected path to logging

--- {{cookiecutter.repo_name}}/src/routes/user.js
+++ {{cookiecutter.repo_name}}/src/routes/user.js
@@ -1,44 +1,44 @@
 import express from 'express';
 import User from '../models/user';
 import {request} from './route_utils';
-import logging from './config/logging';
+import logging from '../config/logging';
 
 const logger = logging(module);
-const router = express.Router ();
+const router = express.Router();
 
 class UserRouter {
-  @request (router.post.bind (router), '/')
-  static async create (req, res) {
+  @request(router.post.bind(router), '/')
+  static async create(req, res) {
     let body = req.body;
-    let user = new User (body);
-    await user.save ();
+    let user = new User(body);
+    await user.save();
     return {user: user};
   }
 
-  @request (router.get.bind (router), '/')
-  static async getAll (req, res) {
-    let users = await User.find ();
+  @request(router.get.bind(router), '/')
+  static async getAll(req, res) {
+    let users = await User.find();
     return {users};
   }
 
-  @request (router.post.bind (router), '/login')
-  static async getFromLogin (req, res) {
+  @request(router.post.bind(router), '/login')
+  static async getFromLogin(req, res) {
     let {email, password} = req.body;
-    console.log (email, password);
-    let user = await User.findOne ({email, password});
+    console.log(email, password);
+    let user = await User.findOne({email, password});
 
     return {user};
   }
 
-  @request (router.get.bind (router), '/:id')
-  static async get (req, res) {
-    let user = await User.findById (req.params.id);
+  @request(router.get.bind(router), '/:id')
+  static async get(req, res) {
+    let user = await User.findById(req.params.id);
     return {user};
   }
 
-  @request (router.patch.bind (router), '/:id')
-  static async update (req, res) {
-    let user = await User.findById (req.params.id);
+  @request(router.patch.bind(router), '/:id')
+  static async update(req, res) {
+    let user = await User.findById(req.params.id);
     return {user};
   }
 }
